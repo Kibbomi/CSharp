b43f10e93cfd74b2dd11b4185df37f00d55cd8c1
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-18 16:49:26 +0900

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    class Switch
+    {
+        static void Main(string[] args)
+        {
+            short item = 14;
+
+            //general switch 
+            switch (item)
+            {
+                case 1:
+                    //do somthing
+                    break;
+
+                case 2:
+                    //do somthing
+                    break;
+
+                default:
+                    //do somthing
+                    break; 
+            }
+
+
+
+            // switch which has type check & condition check
+            object obj = item;
+            switch (obj)
+            {
+                case int i:
+                    Console.WriteLine("This is a integer");
+                    break;
+
+                case float f:
+                    Console.WriteLine("This is a float");
+                    break;
+
+                case short s when s >=10:
+                    Console.WriteLine("This is a short which is greater than 10");
+                    break;
+            }
+
+        }
+    }
+}
