819655fcf9989f645d6c9fd72104ecf4cc80cd4b
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-18 15:32:51 +0900

    Add files via upload

---
+++ Enum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    //class 한정자와 동일한 의미
+    //namespace에 바로 위치할 수 있음.
+    enum Date : int
+    {
+        SUN,//0
+        MON,//1
+        TUE,//2
+        WED = 5,
+        THU,//6
+        FRI,//7
+        SAT//8
+    }
+
+    enum Color { Red, Green, Blue} // default : int
+
+    class Enum
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
