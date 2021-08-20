04c11e0a97c5f765d95d969c7f74fe43c5252124
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-20 17:20:36 +0900

    Add files via upload

---
+++ AnonymousType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{   
+    class AnonymousType
+    {
+        static void Main(string[] args)
+        {
+            var item = new { damage = 1, speed = 5 };
+
+            //anonymous type -> READ ONLY!!!
+            Console.WriteLine(item.damage);
+            Console.WriteLine(item.speed);
+
+            //item.speed = 100 -> ERROR!
+        }
+    }
+}
