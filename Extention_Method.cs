6fbe05ee22ac006dd6096382f6f5c633c828c552
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-20 14:42:40 +0900

    Add files via upload

---
+++ Extention_Method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    //static class !!!
+    static class MyExtention
+    { 
+        //this가 오고 우측에는 매개변수목록이 옴.
+        public static int Power(this int num, int exp)
+        {
+            for (int i = 0; i < exp; ++i)
+                num *= num;
+            return num;
+        }
+    }
+
+    class Extention_Method
+    {
+        static void Main(string[] args)
+        {
+            //이렇게 사용할 수 있음.
+            Console.WriteLine(2.Power(3));
+
+            //static class
+            //static function & this modifier is placed as first argument.
+        }
+    }
+}
