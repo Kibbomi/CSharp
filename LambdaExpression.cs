f01ebb66083b0a4685271c0195894ab069795f01
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-23 13:14:42 +0900

    Add files via upload

---
+++ LambdaExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    delegate int Sum(int a, int b);
+
+    class LambdaExpression
+    {
+        static void Main(string[] args)
+        {
+            // Anonymous Functions
+
+            // using lambda ( C# 3.0 )
+            //Sum myFunc1 = (int a, int b) =>
+            Sum myFunc1 = (a, b) =>
+            {
+                return a + b;
+            };
+
+            // using delegate ( C# 2.0 )
+            Sum myFunc2 = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            Console.WriteLine(myFunc1(1, 2));
+            Console.WriteLine(myFunc2(2, 3));
+
+
+            // -> We must generate delegate types and instances. (not good. not productive)
+        }
+    }
+}
