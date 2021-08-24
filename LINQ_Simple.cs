87516a2caf9f09194079249494b0ea13cbd21d2a
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-24 20:21:32 +0900

    Add files via upload

---
+++ LINQ_Simple.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp
+{
+    class LINQ_Simple
+    {
+        static void Main(string[] args)
+        {
+            // The Three Parts of a LINQ Query :
+            // 1. Data source.
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+            /* from : choose data source
+             * where : condition
+             * select : return type
+             */
+            foreach (int num in numQuery)
+                Console.WriteLine(num);
+            
+            List<int> numQuery2 =
+                (from num in numbers
+                 where (num % 2) == 0
+                 select num).ToList();
+
+            for (int i = 0; i < numQuery2.Count; ++i)
+                Console.WriteLine(numQuery2[i]);
+        }
+    }
+}
+ 
