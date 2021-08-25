8f65bd29c3adabdb02e564ad2849165ef4b8478d
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-08-25 15:11:51 +0900

    Add files via upload

---
+++ Socket_Echo_Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Socket_Echo_Client
+    {
+        static void Main(string[] args)
+        {
+          
+            IPEndPoint ClientAddress = new IPEndPoint(IPAddress.Parse("172.16.163.188"), 0);
+            TcpClient client = new TcpClient(ClientAddress);
+
+            IPEndPoint ServerAddress = new IPEndPoint(IPAddress.Parse("172.16.163.188"), 9876);
+            client.Connect(ServerAddress);
+            Console.WriteLine("Connect to server whose ipAddr is 172.16.163.188:9876");
+
+            try
+            {
+                Console.Write("input string :");
+                string message = Console.ReadLine();
+
+                byte[] data = Encoding.Default.GetBytes(message);
+
+                NetworkStream ns = client.GetStream();
+                ns.Write(data, 0, data.Length);
+
+                Console.WriteLine($"Send : {message}");
+
+                data = new byte[256];
+                string responseData = "";
+                int bytes = ns.Read(data, 0, data.Length);
+                responseData = Encoding.Default.GetString(data, 0, bytes);
+                Console.WriteLine($"Receive : {responseData}");
+
+                ns.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Client is terminated");
+
+        }
+    }
+}
---
+++ Socket_Echo_Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSharp
+{
+    class Socket_Echo_Server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint localAddress = new IPEndPoint(IPAddress.Parse("172.16.163.188"), 9876);
+
+            TcpListener server = new TcpListener(localAddress);
+
+            server.Start();
+            Console.WriteLine("Echo Server On");
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();    //blocked... It waits client's connection
+
+                    NetworkStream ns = client.GetStream();
+
+                    int length;
+                    string data = null;
+                    byte[] bytes = new byte[256];
+
+                    while ((length = ns.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.Default.GetString(bytes, 0, length);
+                        Console.WriteLine($"수신 : {data}");
+
+                        byte[] msg = Encoding.Default.GetBytes(data);
+                        ns.Write(msg, 0, msg.Length);
+                        Console.WriteLine($"송신 : {data}");
+                    }
+                    ns.Close();
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
